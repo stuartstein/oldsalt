0a298164cf608f62222e20c3c036e951d30e9da5
stuartstein <stuart.stein@gmail.com>
2014-02-05 13:07:21 -0800

    Making secrets

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Oldsalt::Application.config.secret_key_base = '6ffb954929e0497584e1770411db31c8968e2edd0ef2f3f834a2e753cb2babea7d581608f1253ab5c6e662d8ba89919c50343f2ee575dba731e28cc90c5764fe'
+Oldsalt::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
